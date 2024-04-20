14dceb6ae7a0f8e2e8ef6d0496c3b40b351bfb95
Jesh <joeshuabaker@gmail.com>
2024-04-19 18:25:42 -0700

    added red pass thru shader for spooky red eye effect in fog, also removed start and update function from tiles to remove redundant calls since their script is a data script

--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -6,15 +6,4 @@ public class Tile : MonoBehaviour
 {
     public bool startTile;
     public bool endTile;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
