da4e41d4a86bb420efec7be8b90c32fea0d51473
PissClown <37281241+PissClown@users.noreply.github.com>
2024-11-01 03:30:24 -0700

    main menu ui

---
+++ Assets/MainMenuUI.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+
+    public GameObject iconContainer;
+    public GameObject discordIcon;
+    public GameObject discordText;
+    public GameObject twitterIcon;
+    public GameObject twitterText;
+    public GameObject steamIcon;
+    public GameObject steamText;
+
+    public GameObject[] icons;
+    public GameObject[] iconsText;
+    public GameObject[] uiArms;
+
+    public float bounceIcon;
+    public float bounceText;
+
+    public float bounceOffset = 0.1f;
+    public float bounceOffsetText = 0.15f;
+    public float bounceDistance = 100f;
+
+    public float bounceSpeed = 1f;
+    public float bounceSpeedArms = .5f;
+    public float bounceDistanceArms = 3f;
+    public float bounceOffsetArms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < icons.Length; i++)
+        {
+            Vector3 bounceAmount = new Vector3(0f, -bounceDistance * Mathf.Sin((Time.time * bounceSpeed) + (bounceOffset * (i +1))), 0f);
+
+            icons[i].transform.localPosition = bounceAmount;
+        }
+
+        for (int i = 0; i < iconsText.Length; i++)
+        {
+            Vector3 bounceAmount = new Vector3(0f, -bounceDistance * Mathf.Sin((Time.time * bounceSpeed) + (bounceOffset * (i + 1) + bounceOffsetText)), 0f);
+
+            iconsText[i].transform.localPosition = bounceAmount;
+        }
+
+        for (int i = 0; i < uiArms.Length; i++)
+        {
+            float sinBounce = -bounceDistanceArms * Mathf.Sin((Time.time * bounceSpeedArms) + (bounceOffsetArms * (i + 1) + bounceOffsetText));
+
+            Vector3 bounceAmount = new Vector3(sinBounce * 0.5f, sinBounce, 0f);
+
+            //uiArms[i].transform.localPosition = bounceAmount;
+
+            var rot = uiArms[i].transform.localRotation;
+
+            rot.eulerAngles = new Vector3(0f, 0f, sinBounce * 0.15f);
+
+            uiArms[i].transform.localRotation = rot;
+        }
+
+        Vector3 bounce = new Vector3(0f, (-bounceDistance *2f) * Mathf.Sin((Time.time * (bounceSpeed) + (bounceOffset * (3) + bounceOffsetText))), 0f);
+
+        steamText.transform.localPosition = bounce;
+
+    }
+}
