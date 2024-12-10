b092a3ee1e9e39ee37a0f67d1faee9fcdb7e5a96
Till-er <74216673+Till-er@users.noreply.github.com>
2024-12-09 23:13:20 -0800

    Audio Update
    -Added missile launch and missile explode sounds
    -Item use no longer plays if active item inflicts any dmg

--- Assets/Till Files/Scripts/AnimationAudioEvent.cs
+++ Assets/Till Files/Scripts/AnimationAudioEvent.cs
@@ -9,4 +9,24 @@ public class AnimationAudioEvent : MonoBehaviour
         AkSoundEngine.PostEvent("PlayPathArrow", this.gameObject);
         return;
     }
+
+    public void PlayBossWarningIntroG()
+    {
+        AkSoundEngine.PostEvent("PlayBossWarningG", this.gameObject);
+        return;
+    }
+
+    public void PlayBossWarningIntroC()
+    {
+        AkSoundEngine.PostEvent("PlayBossWarningC", this.gameObject);
+        return;
+    }
+
+    public void PlayBossSeedWarning()
+    {
+        AkSoundEngine.PostEvent("PlayBossSeedWarning", this.gameObject);
+        return;
+    }
+
+
 }
